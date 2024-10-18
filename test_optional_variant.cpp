2c1a1cd553571362f75d8d52e67b444095c70222
somya-weilliptic <somya@weilliptic.com>
2024-10-18 11:40:24 +0530

    not much useful functions

---
+++ test_optional_variant.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <optional>
+#include <variant>
+#include <string>
+
+std::variant<int, double, std::string> processValue(const std::optional<int>& opt) {
+    if (opt.has_value()) {
+        if (opt.value() % 2 == 0) {
+            return opt.value();
+        } else {
+            return 3.14;
+        }
+    }
+    return "No value";
+}
+
+struct node{
+    int n;
+    std::string s;
+};
+
+void check(std::optional<int> n){
+    int val = n.value_or(2);
+    node n1{
+        val,
+        "somya"
+    };
+
+    std::cout<<n1.n<<std::endl;
+}
+
+
+int main() {
+    // std::optional<int> opt1 = 42;
+    // std::optional<int> opt2 = 15;
+    // std::optional<int> opt3;
+
+    // auto result1 = processValue(opt1);
+    // auto result2 = processValue(opt2);
+    // auto result3 = processValue(opt3);
+
+    // std::visit([](auto&& arg) {
+    //     std::cout << "Result1 holds: " << arg << '\n';
+    // }, result1);
+
+    // std::visit([](auto&& arg) {
+    //     std::cout << "Result2 holds: " << arg << '\n';
+    // }, result2);
+
+    // std::visit([](auto&& arg) {
+    //     std::cout << "Result3 holds: " << arg << '\n';
+    // }, result3);
+
+    int a;
+    int b = 7;
+
+    check(a);
+    check(b);
+
+    return 0;
+}
