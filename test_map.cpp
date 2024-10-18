4b4045e00e643b6add3fe344160490a031227a4a
somya-weilliptic <somya@weilliptic.com>
2024-10-18 11:09:26 +0530

    checking how defaults work in c++

--- test_map.cpp
+++ test_map.cpp
@@ -1,10 +1,29 @@
 #include <map>
 #include <iostream>
+#include <typeinfo>
+
+struct person{
+    int id;
+    std::string name;
+};
+
+struct node{
+    int number;
+    std::string name;
+    std::vector<int> v;
+    person p;
+};
 
 int main(){
-    std::map<int,int> mp;
-    mp[19]=1;
-    int x = mp[10];
+    std::map<int,node> mp;
+    // node n = mp[10];
 
-   std::cout<<x<<std::endl;
+//    std::cout<<n.number<<std::endl;
+
+node n;
+   std::cout<<n.p.name<<std::endl;
+
+//    std::cout << "Type of a: " << typeid(n).name() << std::endl;
+//    std::cout << "Type of a: " << typeid(n.name).name() << std::endl;
+
 }
